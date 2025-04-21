e04b7452932186bc511366da6ba1ff10e85aba00
KingDes <desmondtate@yahoo.com>
2025-04-20 22:51:47 -0400

    Sprint5part2

--- scripts/index.js
+++ scripts/index.js
@@ -2,28 +2,28 @@ const initialCards = [
   {
     name: "Val Thorens"
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg"
-  },
+  }
   {
     name: "Restaurant terrace"
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg"
-  },
+  }
   {
     name: "An outdoor cafe"
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg"
-  },
+  }
   {
     name: "A very long bridge, over the forest and through the trees"
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg"
-  },
+  }
   {
     name: "Tunnel with morning light"
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg"
 
-  },
+  }
   {
     name: "Mountain house"
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg"
-  },
+  }
 ];
 
 const editProfileBtn = document.querySelector(".profile__edit-btn");
@@ -44,28 +44,40 @@ const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
 const profileNameEl = document.querySelector(".profile__name");
 const profileDescriptionEl = document.querySelector(".profile__description");
 
+function openModal(modal) {
+  modal.classList.add("modal_is-opened");
+}
+
+function closeModal(modal) {
+  modal.classList.remove("modal_is-opened");
+}
+
 editProfileBtn.addEventListener("click", function () {
   editProfileNameInput.value = profileNameEl.textContent;
   editProfileDescriptionInput.value = profileDescriptionEl.textContent;
-  editProfileModal.classList.add("modal_is-opened");
+  openModal(editProfileModal);
 });
 
 editProfileCloseBtn.addEventListener("click", function () {
-  editProfileModal.classList.remove("modal_is-opened");
+  closeModal(editProfileModal);
 });
-
+function openModal(modal) {
+  modal.classList.add("modal_is-opened");
+}
 newPostBtn.addEventListener("click", function () {
-  newPostModal.classList.add("modal_is-opened");
+  openModal(newPostModal);
 });
 
 newPostCloseBtn.addEventListener("click", function () {
-  newPostModal.classList.remove("modal_is-opened");
+  closeModal(newPostModal);
 });
+
+
 function handleEditProfileSubmit(evt) {
   evt.preventDefault();
   profileNameEl.textContent = editProfileNameInput.value;
   profileDescriptionEl.textContent = editProfileDescriptionInput.value;
-  editProfileModal.classList.remove("modal_is-opened");
+  closeModal(editProfileModal);
 }
 
 editProfileForm.addEventListener("submit", handleEditProfileSubmit);
@@ -80,7 +92,7 @@ function handleAddCardSubmit(evt) {
   const imageUrl = cardImageInput.value;
   const caption = cardCaptionInput.value;
   createCard(imageUrl, caption);
-  newPostModal.classList.remove("modal_is-opened");
+  closeModal(newPostModal);
   newPostForm.reset();
 }
 newPostForm.addEventListener("submit", handleAddCardSubmit);
